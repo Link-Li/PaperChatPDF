a2dbea13357d25709f25151c371f843630e22bc5
noahzhenli <noahzhenli@tencent.com>
2024-01-04 19:14:49 +0800

    准备合并一下PDF的输出结果

---
+++ code/1_PDF_parse/3_PyMuPDF/3_pdf_format.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+@Time        : 2024/1/3 21:19
+@Author      : noahzhenli
+@Email       : 
+@Description : 将上一步获取的PDF数据进行拼装成格式化的
+{
+"title": xxx
+"section1": {"section2": xxx}
+"section1": xxx
+}
+"""
